08a3ccdc5ab816b749e426e96ba54fb575b019ac
XiaoXiaoFan1995 <614664248@qq.com>
2018-11-15 10:33:58 +0800

    561_Array Partition

---
+++ 561_Array Partition I/561_solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        result=sum(nums[::2])
+        return result
